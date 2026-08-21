2e2a56aa3a665f5e704ef977bf4e4769f0bed498
briandang-wanco <brian.dang@wanco.com>
2026-08-21 11:20:20 -0600

    Cleaned Up Website and Added a Name and Introduction

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Brian Dang</h1>
+        <p>
+          Welcome to my personal website! I am a software engineer with a passion for building web applications and exploring new technologies. Feel free to explore my projects and get in touch!
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
