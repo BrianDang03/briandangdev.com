465516d7b4cd6bf365b399799021f383942f7ae1
briandang-wanco <brian.dang@wanco.com>
2026-08-21 12:41:17 -0600

    Updated Intro

--- src/data/HeroSectionData.ts
+++ src/data/HeroSectionData.ts
@@ -1,5 +1,5 @@
 export const HeroSectionData = {
   title: "Brian Dang",
   jobTitle: "Software Engineer",
-  introduction: "Welcome to my personal website! I am a software engineer with a passion for building web applications and exploring new technologies. Feel free to explore my projects and get in touch!"
+  introduction: "Welcome to my personal website!"
 };
